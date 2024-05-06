46f06f3aa87230e9db48a22f9861902fc07ef28f
LoneWolfXVII <nischal.ngupta11@gmail.com>
2024-05-07 00:54:37 +0530

    comming soon

--- src/components/features/dashboard/page.jsx
+++ src/components/features/dashboard/page.jsx
@@ -1,5 +1,14 @@
 const Dashboard = () => {
-	return <div>Dashboard Coming soon.</div>;
+	return (
+		<div className="w-full h-[88vh] flex flex-col items-center justify-center">
+			<img
+				src="https://dashboard-irame.s3.ap-south-1.amazonaws.com/Site+Stats+Concept+3.svg"
+				alt="Dashboard"
+				className="w-1/2 h-auto"
+			/>
+			<p className="font-medium text-xl text-black/60 mt-5">Coming soon...</p>
+		</div>
+	);
 };
 
 export default Dashboard;
