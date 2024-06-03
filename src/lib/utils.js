e5b5c4f0835d3bc219c490f64910575b89d1b8d0
LoneWolfXVII <nischal.ngupta11@gmail.com>
2024-06-04 00:39:02 +0530

    parser

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -43,3 +43,10 @@ export const getInitials = (userName) => {
 
 	return abbreviation;
 };
+
+export function toTitleCase(str) {
+	if (!str) return str;
+	return str.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+}
