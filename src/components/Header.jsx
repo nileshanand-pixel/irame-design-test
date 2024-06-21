e4150f88fe747739c0c0270e3b99d2a250e5444d
LoneWolfXVII <nischal.ngupta11@gmail.com>
2024-06-22 00:06:04 +0530

    trying logout changes --replce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
 							<DropdownMenuItem
 								className="text-primary100 text-sm font-medium"
 								onClick={() =>
-									(window.location.href = `${API_URL}/oauth/google/logout`)
+									(window.location.replace = `${API_URL}/oauth/google/logout`)
 								}
 							>
 								<i className="bi-box-arrow-left mr-2 text-primary100"></i>
