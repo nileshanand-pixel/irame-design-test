333616b0c4fe8194820582a7d93404a312ae8c8e
LoneWolfXVII <nischal.ngupta11@gmail.com>
2024-06-29 13:54:04 +0530

    collect intent while creating datasource

---
+++ src/components/features/configuration/configuration.content.ts
@@ -0,0 +1,46 @@
+export const intent = [
+	{
+		label: 'Adhoc analysis',
+		value: 'adhoc_analysis',
+	},
+	{
+		label: 'Claim validation',
+		value: 'claim_validation',
+	},
+	{
+		label: 'Reconciliation',
+		value: 'reconciliation',
+	},
+	{
+		label: 'Contract validation',
+		value: 'contract_validation',
+	},
+	{
+		label: 'KPI Monitoring',
+		value: 'kpi_monitoring',
+	},
+	{
+		label: 'Process compliance',
+		value: 'process_compliance',
+	},
+	{
+		label: 'RFP management',
+		value: 'rfp_management',
+	},
+	{
+		label: 'Expense validation',
+		value: 'expense_validation',
+	},
+	{
+		label: 'Invoice validation',
+		value: 'invoice_validation',
+	},
+	{
+		label: 'Fraud monitoring',
+		value: 'fraud_monitoring',
+	},
+	{
+		label: 'Other',
+		value: 'other',
+	},
+];
