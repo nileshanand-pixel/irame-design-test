4afc1eb1f6008613fa72269ac1720bb0f153f841
Kuldeep Pandey <kuldeep.msvm@gmail.com>
2024-08-15 17:20:57 +0530

    mind fuck

--- src/components/features/new-chat/Workspace.jsx
+++ src/components/features/new-chat/Workspace.jsx
@@ -30,13 +30,10 @@ const Workspace = ({ handleTabClick, workspace, answerResp, setWorkspace }) => {
 		switch (workspace?.activeTab) {
 			case WorkspaceEnum.Planner:
 				return (
-					<WorkspaceEditProvider>
-						<PlannerComponent
-							data={answerResp?.answer?.[WorkspaceEnum.Planner]}
-							status={answerResp?.status}
-							setHasAnyChanges = {setHasAnyChanges}
-						/>
-					</WorkspaceEditProvider>
+					<PlannerComponent
+						data={answerResp?.answer?.[WorkspaceEnum.Planner]}
+						status={answerResp?.status}
+					/>
 				);
 			case WorkspaceEnum.Coder: {
 				const coderData = answerResp?.answer?.[WorkspaceEnum.Coder];
@@ -53,14 +50,11 @@ const Workspace = ({ handleTabClick, workspace, answerResp, setWorkspace }) => {
 			case WorkspaceEnum.Answer:
 			case WorkspaceEnum.Reference:
 				return (
-					<WorkspaceEditProvider>
-						<SourceComponent
-							data={answerResp?.answer?.[workspace.activeTab]}
-							datasourceId={answerResp?.datasource_id}
-							status={answerResp?.status}
-							setHasAnyChanges = {setHasAnyChanges}
-						/>
-					</WorkspaceEditProvider>
+					<SourceComponent
+						data={answerResp?.answer?.[workspace.activeTab]}
+						datasourceId={answerResp?.datasource_id}
+						status={answerResp?.status}
+					/>
 				);
 			default:
 				return null;
@@ -68,76 +62,76 @@ const Workspace = ({ handleTabClick, workspace, answerResp, setWorkspace }) => {
 	}, [workspace?.activeTab, answerResp?.answer]);
 
 	return (
-		<div className=" rounded-2xl my-6 w-[100%] h-full overflow-hidden relative ">
-			<ul className="ghost-tabs relative col-span-12 mb-4 inline-flex w-full border-b border-black-10">
-				{answerResp?.answer
-					? Object.keys(answerResp?.answer)
-							?.filter(
-								(key) =>
-									answerResp?.answer[key]?.tool_space ===
-									'secondary',
-							)
-							.map((items, indx) => (
-								<li
-									key={indx}
-									className={[
-										'!pb-0 flex items-center gap-2',
-										workspace.activeTab === items
-											? 'active-tab'
-											: 'default-tab',
-									].join(' ')}
-									onClick={() => handleTabClick(items)}
-								>
-									{workSpaceMap[items]}
-									{workspace.visitedTabs[items] ? null : (
-										<span className="relative flex size-2 ">
-											<span className="absolute inline-flex h-full w-full rounded-full bg-purple-100"></span>
-											<span className="animate-ping relative inline-flex rounded-full size-2 bg-purple-100"></span>
-										</span>
-									)}
-								</li>
-							))
-					: [...Array(4)]?.map((_, index) => (
-							<Skeleton
-								key={index}
-								className="h-4 w-[150px] space-x-2 mr-2 bg-purple-8 rounded-lg mb-1"
-							/>
-						))}
-			</ul>
-			{answerResp?.answer ? (
-				<div className="w-full h-[95%] overflow-y-auto">
-					{renderedComponent}
-					<div className="my-2 flex gap-4 w-full absolute bottom-10">
-						<Button
-							variant="outline"
-							className="text-muted-foreground cursor-pointer w-1/2"
-							onClick={() => alert('implement reset')}
-							disabled={!hasAnyChanges}
-						>
-							Reset
-						</Button>
-						<Button
-							className="rounded-lg hover:bg-purple-100 hover:text-white hover:opacity-80 w-1/2"
-							onClick={() => {
-								alert('implement Regenerate Response');
-							}}
-							disabled={!hasAnyChanges}
-						>
-							Regenerate Response
-						</Button>
+		<WorkspaceEditProvider>
+			<div className=" rounded-2xl my-6 w-[100%] h-full overflow-hidden relative ">
+				<ul className="ghost-tabs relative col-span-12 mb-4 inline-flex w-full border-b border-black-10">
+					{answerResp?.answer
+						? Object.keys(answerResp?.answer)
+								?.filter(
+									(key) =>
+										answerResp?.answer[key]?.tool_space ===
+										'secondary',
+								)
+								.map((items, indx) => (
+									<li
+										key={indx}
+										className={[
+											'!pb-0 flex items-center gap-2',
+											workspace.activeTab === items
+												? 'active-tab'
+												: 'default-tab',
+										].join(' ')}
+										onClick={() => handleTabClick(items)}
+									>
+										{workSpaceMap[items]}
+										{workspace.visitedTabs[items] ? null : (
+											<span className="relative flex size-2 ">
+												<span className="absolute inline-flex h-full w-full rounded-full bg-purple-100"></span>
+												<span className="animate-ping relative inline-flex rounded-full size-2 bg-purple-100"></span>
+											</span>
+										)}
+									</li>
+								))
+						: [...Array(4)]?.map((_, index) => (
+								<Skeleton
+									key={index}
+									className="h-4 w-[150px] space-x-2 mr-2 bg-purple-8 rounded-lg mb-1"
+								/>
+							))}
+				</ul>
+				{answerResp?.answer ? (
+					<div className="w-full h-[95%] overflow-y-auto">
+						{renderedComponent}
+						<div className="my-2 flex gap-4 w-full absolute bottom-10">
+							<Button
+								variant="outline"
+								className="text-muted-foreground cursor-pointer w-1/2"
+								onClick={() => alert('implement reset')}
+							>
+								Reset
+							</Button>
+							<Button
+								className="rounded-lg hover:bg-purple-100 hover:text-white hover:opacity-80 w-1/2"
+								onClick={() => {
+									alert('implement Regenerate Response');
+								}}
+							>
+								Regenerate Response
+							</Button>
+						</div>
 					</div>
-				</div>
-			) : (
-				// <Skeleton className="h-96 w-full bg-purple-8 rounded-2xl" />
-				<div className="flex flex-col space-y-3">
-					<div className="space-y-2">
-						<Skeleton className="h-5 w-[50%] bg-purple-8" />
-						<Skeleton className="h-5 w-[90%] bg-purple-8" />
+				) : (
+					// <Skeleton className="h-96 w-full bg-purple-8 rounded-2xl" />
+					<div className="flex flex-col space-y-3">
+						<div className="space-y-2">
+							<Skeleton className="h-5 w-[50%] bg-purple-8" />
+							<Skeleton className="h-5 w-[90%] bg-purple-8" />
+						</div>
+						<Skeleton className="h-[125px] w-[250px] rounded-xl bg-purple-8" />
 					</div>
-					<Skeleton className="h-[125px] w-[250px] rounded-xl bg-purple-8" />
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</WorkspaceEditProvider>
 	);
 };
 
