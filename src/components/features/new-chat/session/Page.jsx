8616a70d502a04cfcadc565180f8dc22a8a9dc94
Harsh-garg-coder <garg.harsh0014@gmail.com>
2024-09-22 19:36:10 +0530

    Resolved some issues of workspace

--- src/components/features/new-chat/session/Page.jsx
+++ src/components/features/new-chat/session/Page.jsx
@@ -466,8 +466,7 @@ const Workzone = () => {
 			const currentDoingScience =
 				doingScience.find((loadingObj) => loadingObj.queryId === query?.id)
 					?.status || !!query?.parentQueryId;
-			const showWorkspaceToggle =
-				!hasClarification && answerElem?.type === 'single';
+			const showWorkspaceToggle = !hasClarification ;
 			return (
 				<div key={query.id} className="my-2 w-full">
 					<div
@@ -666,7 +665,7 @@ const Workzone = () => {
 		// 	secondaryItems?.length > 1 || markerAnswer?.status === 'done';
 		return (
 			workspace.show &&
-			markerAnswer?.type === 'single' &&
+			// markerAnswer?.type === 'single' &&
 			!hasClarification
 		);
 	};
