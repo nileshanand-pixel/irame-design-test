a1e63fb9a29479d70790169d8ad38134cacb1165
tushar-irame <tushar@irame.ai>
2024-11-27 19:09:27 +0530

    added type

--- src/components/features/new-chat/session/Page.jsx
+++ src/components/features/new-chat/session/Page.jsx
@@ -360,7 +360,10 @@ const Workzone = () => {
 					query: tempPrompt,
 					session_id: answer?.session_id,
 					workspace_changes: workspaceChanges.apiConfig,
-					metadata: workspaceChanges.metadata,
+					metadata: {
+						queries: answer?.metadata?.queries.filter((query) => query?.text?.length > 0).map((item)=> ({query: item?.text})),
+						saved_query_reference: answer?.metadata?.savedQueryReference
+					},
 					type: answer?.type,
 				},
 				getToken(),
