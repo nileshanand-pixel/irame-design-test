608b089f382e6b0ebfb438a081f3b83e28cff15f
Kuldeep Pandey <kuldeep.msvm@gmail.com>
2024-12-23 00:27:46 +0530

    [SCUMR-72]: Terms and conditions accept

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,4 @@
+// src/AppRoutes.js
 import { Navigate, Route, Routes } from 'react-router-dom';
 import SignInSignUp from '@/components/features/login/page';
 import NewChat from '@/components/features/new-chat/page';
@@ -8,63 +9,82 @@ import Configuration from '@/components/features/configuration/page';
 import Workzone from '@/components/features/new-chat/session/Page';
 import DashboardDetailsPage from '@/components/features/dashboard/components/DashboardDetailsPage';
 import TestRoute from '@/components/features/testingUI/Page';
-import ProtectedRoute from './ProtectedRoute'; // Import the new ProtectedRoute component
+import ProtectedRoute from './ProtectedRoute';
 import Reports from '@/components/features/reports/Page';
+import TermsModal from '@/components/TermsModal';
 
 const AppRoutes = () => {
 	return (
-		<Routes>
-			<Route exact path="/*" element={<SignInSignUp />} />
-			<Route
-				path="/app/*"
-				element={
-					<Layout>
-						<Routes>
-							<Route path="/" element={<Navigate to="new-chat" />} />
-							<Route
-								path="new-chat/session"
-								element={<ProtectedRoute element={<Workzone />} />}
-							/>
-							<Route
-								path="new-chat/*"
-								element={<ProtectedRoute element={<NewChat />} />}
-							/>
-							<Route
-								path="dashboard"
-								element={<ProtectedRoute element={<Dashboard />} />}
-							/>
-							<Route
-								path="dashboard/content"
-								element={
-									<ProtectedRoute
-										element={<DashboardDetailsPage />}
-									/>
-								}
-							/>
-							<Route
-								path="dashboard/*"
-								element={<ProtectedRoute element={<Dashboard />} />}
-							/>
-							<Route
-								path="configuration"
-								element={
-									<ProtectedRoute element={<Configuration />} />
-								}
-							/>
-							<Route
-								path="help"
-								element={<ProtectedRoute element={<Help />} />}
-							/>
-							<Route
-								path="reports/*"
-								element={<ProtectedRoute element={<Reports />} />}
-							/>
-						</Routes>
-					</Layout>
-				}
-			/>
-			<Route path="test" element={<TestRoute />} />
-		</Routes>
+		<>
+			<TermsModal />
+			<Routes>
+				<Route exact path="/*" element={<SignInSignUp />} />
+				<Route
+					path="/app/*"
+					element={
+						<Layout>
+							<Routes>
+								<Route
+									path="/"
+									element={<Navigate to="new-chat" />}
+								/>
+								<Route
+									path="new-chat/session"
+									element={
+										<ProtectedRoute element={<Workzone />} />
+									}
+								/>
+								<Route
+									path="new-chat/*"
+									element={
+										<ProtectedRoute element={<NewChat />} />
+									}
+								/>
+								<Route
+									path="dashboard"
+									element={
+										<ProtectedRoute element={<Dashboard />} />
+									}
+								/>
+								<Route
+									path="dashboard/content"
+									element={
+										<ProtectedRoute
+											element={<DashboardDetailsPage />}
+										/>
+									}
+								/>
+								<Route
+									path="dashboard/*"
+									element={
+										<ProtectedRoute element={<Dashboard />} />
+									}
+								/>
+								<Route
+									path="configuration"
+									element={
+										<ProtectedRoute
+											element={<Configuration />}
+										/>
+									}
+								/>
+								<Route
+									path="help"
+									element={<ProtectedRoute element={<Help />} />}
+								/>
+								<Route
+									path="reports/*"
+									element={
+										<ProtectedRoute element={<Reports />} />
+									}
+								/>
+							</Routes>
+						</Layout>
+					}
+				/>
+				<Route path="test" element={<TestRoute />} />
+			</Routes>
+		</>
 	);
 };
 
