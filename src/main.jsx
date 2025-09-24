ae1e0816ea68f599bf9ade5174cbbfc6d903d735
funactuator <kuldeep.msvm@gmail.com>
2025-09-25 02:06:51 +0530

    [sentry-setup]: improved strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	import.meta.env.VITE_DISABLE_REACT_STRICT_MODE ? (
+	import.meta.env.VITE_DISABLE_REACT_STRICT_MODE === 'true' ? (
 		<App />
 	) : (
 		<React.StrictMode>
